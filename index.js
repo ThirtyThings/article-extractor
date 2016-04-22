f2b7e968656d464f9eb0597e259eb0fc816389d6
Mike Reich <mjreich@gmail.com>
2016-04-22 15:12:34 -0700

    added catch for non 200 response

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ module.exports = {
       'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
     }}
     request(articleUrl, opts, function (err, response, body) {
+      if(err) return callback(err)
+      if(res.statusCode != 200) return callback('Invalid response')
       var data = {};
       var preppedHtml = cleaner.prepForParsing(body);
 
