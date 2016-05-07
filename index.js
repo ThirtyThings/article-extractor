aead0ff77d4df896564ead6cbebdb10c8c9fcf1f
Mike Reich <mjreich@gmail.com>
2016-05-07 14:18:23 -0700

    Updated article extractor to support pdf parsing

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ module.exports = {
   extractData: function (articleUrl, callback) {
     var opts = {headers: {
       'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
-    }}
+    },encoding: null}
     request(articleUrl, opts, function (err, response, body) {
-      if(err) return callback(err)
-      if(response.statusCode != 200) return callback('Invalid response: '+response.statusCode)
+      if(err) return callback(err, response, body)
+      if(response.statusCode != 200) return callback('Invalid response: '+response.statusCode, response, body)
+      if(response.headers['content-type'].indexOf("html") == -1) return callback(null, {}, response, body)
+      else body = body.toString();
       var data = {};
       var preppedHtml = cleaner.prepForParsing(body);
       data.domain = url.parse(articleUrl).host;
@@ -21,7 +23,7 @@ module.exports = {
       data.title = title.getTitle(preppedHtml);
       data.content = content.getArticleContent(preppedHtml, data.host);
       data.summary = summary.getSummary(preppedHtml, data.content);
-      callback(null, data);
+      callback(null, data, response, body);
     });
   }
 }
